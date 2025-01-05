c51ea7f52e884e9d0e0b663aaa6be3f588e2bc98
Bram Windey <windey.bram@gmail.com>
2025-01-05 15:42:02 +0100

    Apply the split in offse (char + bytes) in wi_tui.c, should have been in the commit from 3 commits ago, but changing that now would require too much git-trickery

--- src/tui.c
+++ src/tui.c
@@ -40,7 +40,8 @@ wi_window* wi_make_window(void) {
 	window->internal.depending_windows = NULL;
 	window->internal.amount_depending = 0;
 
-	window->internal.content_render_offset = (wi_position) { 0, 0 };
+	window->internal.content_offset_chars = (wi_position) { 0, 0 };
+	window->internal.content_offset_bytes = (wi_position) { 0, 0 };
 	window->internal.visual_cursor_position = (wi_position) { 0, 0 };
 	window->internal.currently_focussed = false;
 
