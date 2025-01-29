13673fe9ecba611e777897e8e65bd0b4ab79c34a
Bram Windey <windey.bram@gmail.com>
2025-01-29 18:14:10 +0100

    Implement modified split_lines() function to use wi_string_view and modified wi_content. Also changed wi_code_lengths to wi_string_lengths

--- src/utility.c
+++ src/utility.c
@@ -1,106 +1,94 @@
+#include "wiAssert.h"
 #include "wi_data.h"
 #include "wi_internals.h"
 
+#include <stdio.h>
 #include <stdlib.h> 	/* malloc(), realloc() */
-#include <string.h>		/* strcpy() */
 
-wi_code_lengths wi_char_byte_size(const char* c) {
+#define INITIALISE_LINE_LIST_EL(i, char_p) \
+	line_list[i].length.width = 0; \
+	line_list[i].length.bytes = 0; \
+	line_list[i].string = char_p;
+
+wi_string_length wi_char_byte_size(const char* c) {
 	if (*c == '\0') {
-		return (wi_code_lengths) { .width = 0, .bytes = 1 };
+		return (wi_string_length) { .width = 0, .bytes = 1 };
 	} else if ((*c & 0xF0) == 0xF0) { 	/* 11110000 */
-		return (wi_code_lengths) { .width = 1, .bytes = 4 };
+		return (wi_string_length) { .width = 1, .bytes = 4 };
 	} else if ((*c & 0xE0) == 0xE0) { 	/* 11100000 */
-		return (wi_code_lengths) { .width = 1, .bytes = 3 };
+		return (wi_string_length) { .width = 1, .bytes = 3 };
 	} else if ((*c & 0xC0) == 0xC0) { 	/* 11000000 */
-		return (wi_code_lengths) { .width = 1, .bytes = 2 };
+		return (wi_string_length) { .width = 1, .bytes = 2 };
 	} else if (*c == '\033') {
 		unsigned short bytes = 1;
 		do {
 			bytes++;
 			c++;
 		} while (*c != 'm' && *c != '\0');
-		return (wi_code_lengths) { .width = 0, .bytes = bytes };
+		return (wi_string_length) { .width = 0, .bytes = bytes };
+	} else {
+		return (wi_string_length) { .width = 1, .bytes = 1 };
 	}
-	return (wi_code_lengths) { .width = 1, .bytes = 1 };
 }
 
-wi_content* split_lines(const char* content) {
-	int internal_amount_lines = 10;
+wi_content split_lines(char* content) {
+	wi_string_view* original = (wi_string*) malloc(sizeof(wi_string));
+	original->string = content;
+	original->length = (wi_string_length) { 0, 0 };
+
 	int amount_lines = 0;
-	char** lines = (char**) malloc(internal_amount_lines * sizeof(char*));
-	unsigned int* line_lengths_bytes = (unsigned int*) malloc(
-		internal_amount_lines * sizeof(unsigned int*)
-	);
-	unsigned int* line_lengths_chars = (unsigned int*) malloc(
-		internal_amount_lines * sizeof(unsigned int*)
+	int line_list_capacity = 10;
+	wi_string_view* line_list = (wi_string_view*) malloc(
+		line_list_capacity * sizeof(wi_string_view)
 	);
 
-	int i = 0;
-	int line_length_bytes = 0;
-	int line_length_chars = 0;
+	/* Initialise */
+	INITIALISE_LINE_LIST_EL(0, original->string)
+
+	int bytes = 0;
+	int chars = 0;
 
 	while (true) {
-		if (content[i] == '\n' || content[i] == '\0') {
-			/* Grow arrays if necessary */
-			if (amount_lines == internal_amount_lines) {
-				internal_amount_lines += 10;
-				lines = (char**) realloc(
-					lines, internal_amount_lines * sizeof(char*)
-				);
-				line_lengths_bytes = (unsigned int*) realloc(
-					line_lengths_bytes, internal_amount_lines * sizeof(unsigned int)
-				);
-				line_lengths_chars = (unsigned int*) realloc(
-					line_lengths_chars, internal_amount_lines * sizeof(unsigned int)
-				);
-			}
-
-			/* Store info in the arrays */
-			if (line_length_chars == 0) {
-				line_lengths_bytes[amount_lines] = 1;
-				line_lengths_chars[amount_lines] = 1;
-				lines[amount_lines] = (char*) malloc(2 * sizeof(char));
-				strcpy(lines[amount_lines], " ");
-			} else {
-				line_lengths_chars[amount_lines] = line_length_chars;
-				line_lengths_bytes[amount_lines] = line_length_bytes;
-				lines[amount_lines] = (char*) malloc(
-					(line_length_bytes + 1) * sizeof(char)
-				);
-				memcpy(
-					lines[amount_lines], 				/* Destination */
-					content + i - line_length_bytes, 	/* Source */
-					line_length_bytes					/* Size */
-				);
-				lines[amount_lines][line_length_bytes] = '\0';
-			}
-
+		if (content[bytes] == '\0') {
 			amount_lines++;
-			line_length_chars = 0;
-			line_length_bytes = 0;
-			if (content[i] == '\0') {
-				break;
+			break;
+		} else if (content[bytes] == '\n') {
+			amount_lines++;
+
+			/* Grow arrays if necessary */
+			if (amount_lines == line_list_capacity) {
+				line_list_capacity *= 2;
+				line_list = (wi_string_view*) realloc(
+					line_list, line_list_capacity * sizeof(wi_string_view)
+				);
+				wiAssert(
+					line_list != NULL,
+					"Failed to grow array when processing window content."
+				);
 			}
-			i++;
+
+			bytes++;
+			chars++;
+
+			/* Initialise current line 1 character behind the newline */
+			INITIALISE_LINE_LIST_EL(amount_lines, content + bytes)
 		} else {
-			wi_code_lengths codepoint_length = wi_char_byte_size(content + i);
-			line_length_bytes += codepoint_length.bytes;
-			i += codepoint_length.bytes;
-			line_length_chars += codepoint_length.width;
+			wi_string_length char_len = wi_char_byte_size(content + bytes);
+			bytes += char_len.bytes;
+			chars += char_len.width;
+			line_list[amount_lines].length.bytes += char_len.bytes;
+			line_list[amount_lines].length.width += char_len.width;
 		}
 	}
 
+	original->length.width = chars;
+	original->length.bytes = bytes;
 
-	wi_content* split_contents = (wi_content*) malloc(sizeof(wi_content));
-	*split_contents = (wi_content){
-		.lines = lines,
-		.line_lengths_bytes = line_lengths_bytes,
-		.line_lengths_chars = line_lengths_chars,
-		.amount_lines = amount_lines,
-		.internal_amount_lines = internal_amount_lines
+	return (wi_content) {
+		.original = original,
+		.line_list = line_list,
+		.amount_lines = amount_lines
 	};
-
-	return split_contents;
 }
 
 void skip_continuation_bytes_left(int* p, const char* c) {
